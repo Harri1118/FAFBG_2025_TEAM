4578b5d24c36b2de4c0e1869675998c80b28db05
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2025-06-25 13:44:47 -0400

    Revert "Adding sectiosn of the ARCE website to master repo"
    This reverts commit 4075b95fad6a2bd240c074d995461500115f82d8.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './main/landing'
-import Explore from './main/explore'
-import References from './main/references'
 import Map from './burial_finder/Map'
-import Tutorial from './main/tutorial'
 function App() {
   return (
-    <Tutorial/>
+    <Map/>
   );
 }
+
 export default App;

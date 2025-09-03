1b92c7b0a4761d380627022ad3dd7616467d042a
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2025-09-03 15:43:37 -0400

    Completed pipeline for webserver

--- pipeline/script.ps1
+++ pipeline/script.ps1
@@ -1,17 +1,61 @@
+param(
+    [string]$Debug
+)
 # Check the current branch
 # Announce to user what branch they're on.
+# Ensure they want to use this branch.
 $currentBranch = git rev-parse --abbrev-ref HEAD
-Write-Host "You are on branch: $currentBranch"
+while ($true) {
+    $answer = Read-host "Are you sure you want to deploy to $currentBranch? Type 'yes' or 'no' (case sensitive) "
+    if ($answer -eq "yes") {
+        break
+    }
+    elseif ($answer -eq "no") {
+        return
+    }
+    else {
+        Write-host "Invalid input"
+    }
+}
+# Test app build before building path.
+if(($Debug -ne "true") -and ($currentBranch -ne "dev")){
+    Write-host "Linting app..."
+    Set-Location ../
+    npm run test
+    if($LASTEXITCODE -eq 0){
+        Write-Host "npm run test passed successfully"
+    }
+    else{
+        Write-Host "npm run test failed with exit code: $LASTEXITCODE. Closing..."
+        return
+    }
+    Set-Location pipeline
+}
 # Test is path build exists, if it doesn't, build it
-Test-Path -Path "../build"
-$localPath = "../build"
+if (-not (Test-Path -Path "../build")) {
+    Write-host "No build file present, building file..."
+    if ($Debug -ne "true") {
+        Set-Location ../
+        npm run build
+        if($LASTEXITCODE -eq 0){
+            Write-Host "npm build complete"
+        }
+        else{
+            Write-Host "Build failed with exit code: $LASTEXITCODE"
+            return
+        }
+        Set-Location pipeline
+    }
+}
+
+$buildPath = "../build"
 # Collect server addr and path from env variables.
 # Read .env file into a hashtable
-$envFile = Get-Content ".env" | Where-Object {$_ -match "="} |
-    ForEach-Object {
-        $name, $value = $_ -split "=", 2
-        @{ Key = $name; Value = $value }
-    }
+$envFile = Get-Content ".env" | Where-Object { $_ -match "=" } |
+ForEach-Object {
+    $name, $value = $_ -split "=", 2
+    @{ Key = $name; Value = $value }
+}
 # Convert to dictionary
 $envDict = @{}
 foreach ($item in $envFile) {
@@ -19,40 +63,51 @@ foreach ($item in $envFile) {
 }
 $Link = $envDict["LINK"]
 $Path = $envDict["PATH"]
-# foreach ($file in $files) {
-#     Write-Host $file.Name
-# }
+foreach ($file in $files) {
+    Write-Host $file.Name
+}
 
 # Collect credentials to user and log in.
 $credential = New-Object System.Management.Automation.PSCredential (Get-Credential)
 $sftpSession = New-SFTPSession -ComputerName $Link -Credential $credential
 # Navigate to sftp path for uploading the files
-$remoteDir = ""
 $remotePath = ""
-if($currentBranch -eq "main"){
+if ($currentBranch -eq "main") {
     $remotePath = "$Path"
 }
-else{
+else {
     $remotePath = "$Path$currentBranch"
 }
-$remoteDir = Get-SFTPChildItem -SFTPSession $sftpSession -Path $remotePath
 
-# Upload directory contents recursively
-Get-ChildItem -Path $localPath -Recurse | ForEach-Object {
-    $localFile  = $_.FullName
-    # need to remove bath 
-    $relativePath = $_.FullName.Substring($localPath.Length).TrimStart('\')
-    $remoteFile   = (Join-Path $remotePath $relativePath) -replace '\\','/'
-    # Use Test-Path to check if the directory exists
-    if (Test-Path -Path $remotePath -PathType Container) {
-        Write-Host "The directory '$remotePath' exists."
-    } else {
-        Write-Host "The directory '$remotePath' does not exist."
+# #Upload directory contents recursively
+Get-ChildItem -Path $buildPath -Recurse | ForEach-Object {
+    # Get full file path, create a relative path in order to separate the contents of the build file.
+    $fullPath = $_.FullName
+    $index = $fullPath.ToLower().IndexOf("build\")
+    if ($index -ge 0) {
+        $relativePath = $fullPath.Substring($index + 6) -replace '\\', '/'
     }
-    if (-not $_.PSIsContainer) {
-        $remoteFile
-        Write-Host "Uploading $localFile to $remoteFile"
-        Set-SFTPItem -Session $sftpSession.SessionID -Destination $remotePath -Path $localFile -verbose -force
+    # Using the relative path, join the remote path with it to establish the proper directory to send the file to.
+    $remoteFile = (Join-Path $remotePath $relativePath) -replace '\\', '/'
+    # Checks if relative path is a directory. If it is then create it in the webserver (if it doesn't exist already)
+    if (Test-Path -Path "../build/$relativePath" -PathType Container) {
+        if ($Debug -ne "true") {
+            New-SFTPItem -SessionId $sftpSession.SessionID -Path "$remotePath/$relativePath" -ItemType Directory -ErrorAction SilentlyContinue
+        }
+    }
+    else {
+        # If the remoteFile path is not a dir, then reformat it to fit the proper path format for the web server to interperet.
+        $remoteFile = Split-Path $remoteFile
+        # Replace backslashes with forward slashes
+        $remoteFile = $remoteFile -replace '\\', '/'
+        # Upload file to user. Force upload regardless if file of same name exists within webserver.
+        Write-Host "Uploading $fullPath to $remoteFile"
+        if ($Debug -ne "true") {
+            Set-SFTPItem -Session $sftpSession.SessionID -Destination $remoteFile -Path $fullPath -verbose -force
+        }
     }
 }
-Remove-SFTPSession -SFTPSession $sftpSession
+$out = Remove-SFTPSession -SFTPSession $sftpSession
+if ($out -ne "true") {
+    $out
+}
